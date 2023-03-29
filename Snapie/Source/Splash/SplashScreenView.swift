59b5e8df2d69445859d8a7425a1e8785c7265e47
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-03-29 18:10:06 +0900

    feat : splash screen

--- Snapie/Source/Splash/SplashScreenView.swift
+++ Snapie/Source/Splash/SplashScreenView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @State private var isActive = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+            MainTabView()
+        } else {
+            ZStack {
+                VStack{
+                    Text("Snapie")
+                        .foregroundColor(.white)
+                }.frame(maxWidth: .infinity,maxHeight: .infinity)
+                .background(Color.primary1)
+                
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
