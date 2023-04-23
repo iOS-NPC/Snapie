7eaaff4bafbb859182a72f47531d4d3b2b605641
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-04-23 23:45:29 +0900

    feat : speech recognizer

---
+++ Snapie/Source/Main/Home/Record/SpeechRecognizer.swift
@@ -0,0 +1,48 @@
+//
+//  SpeechRecognizer.swift
+//  Snapie
+//
+//  Created by 남경민 on 2023/04/12.
+//
+
+import AVFoundation
+import Foundation
+import Speech
+import SwiftUI
+
+class SpeechRecognizer: ObservableObject {
+    
+    private var audioEngine: AVAudioEngine?
+    private var request: SFSpeechAudioBufferRecognitionRequest?
+    private var task: SFSpeechRecognitionTask?
+    private let recognizer: SFSpeechRecognizer?
+    
+    init() {
+        recognizer = SFSpeechRecognizer()
+        
+        /*
+        Task(priority: .background) {
+            do {
+                guard recognizer != nil else {
+                    throw RecognizerError.nilRecognizer
+                }
+                guard await SFSpeechRecognizer.hasAuthorizationToRecognize() else {
+                    throw RecognizerError.notAuthorizedToRecognize
+                }
+                guard await AVAudioSession.sharedInstance().hasPermissionToRecord() else {
+                    throw RecognizerError.notPermittedToRecord
+                }
+            } catch {
+                speakError(error)
+            }
+        }
+         */
+    }
+    func reset() {
+        task?.cancel()
+        audioEngine?.stop()
+        audioEngine = nil
+        request = nil
+        task = nil
+    }
+}
